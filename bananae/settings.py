2f5680055a877b9417aab9b766acc26981314d5f
JunPaynandos <junreypaynandos@gmail.com>
2025-12-26 21:37:13 +0800

    Fix static files config2

--- bananae/settings.py
+++ bananae/settings.py
@@ -277,14 +277,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'escan', 'static'),
+    BASE_DIR / 'escan' / 'static',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
